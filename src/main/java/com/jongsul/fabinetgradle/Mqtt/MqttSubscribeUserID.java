2ea2a0bb8eab4170bffa77f5c737b8d0afae0e55
llshl <lsh970708@naver.com>
2021-05-22 22:56:15 +0900

    update mqtt method

--- src/main/java/com/jongsul/fabinetgradle/Mqtt/MqttSubscribeUserID.java
+++ src/main/java/com/jongsul/fabinetgradle/Mqtt/MqttSubscribeUserID.java
@@ -3,46 +3,51 @@ package com.jongsul.fabinetgradle.Mqtt;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import javax.annotation.PostConstruct;
+
 public class MqttSubscribeUserID extends Thread{
-    private static String userName = "de";
+    private static String userName = "default name";
+    private static String MQTT_BROKER_URI = "tcp://3.34.255.198:1883";
+    private static String SUBSCRIBE_TOPIC = "heum/username";
 
-    public String MqttSub() {
-            String findUserName = "";
-            MqttClient client;
-            try {
-                client = new MqttClient(
-                        "tcp://3.34.255.198:1883", //URI
-                        MqttClient.generateClientId(), //ClientId
-                        new MemoryPersistence()
-                );
-                client.setCallback(new MqttCallback() {
-                    @Override
-                    public void connectionLost(Throwable cause) { //Called when the client lost the connection to the broker
-                        System.out.println("MQTT Connection Lost");
-                    }
+    @PostConstruct
+    public void MqttSub() {
+        System.out.println("Mqtt 메서드 실행");
+        String findUserName = "";
+        MqttClient client;
+        try {
+            client = new MqttClient(
+                    MQTT_BROKER_URI, //URI
+                    MqttClient.generateClientId(), //ClientId
+                    new MemoryPersistence()
+            );
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) { //Called when the client lost the connection to the broker
+                    System.out.println("MQTT Connection Lost");
+                }
 
-                    @Override
-                    public void messageArrived(String topic, MqttMessage message) throws Exception {
-                        System.out.println("published topic :" + topic + " ,content: " + message.toString());
-                        userName = message.toString();
-                        //이 메시지 인자에 유저사진명(ID)가 넘어오므로 이것을 사용해서 DB조회하자
-                    }
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    System.out.println("published topic :" + topic + " ,content: " + message.toString());
+                    userName = message.toString();
+                    //이 메시지 인자에 유저사진명(ID)가 넘어오므로 이것을 사용해서 DB조회하자
+                    //여기에 디비조회 넣자
+                }
 
-                    @Override
-                    public void deliveryComplete(IMqttDeliveryToken token) {//Called when a outgoing publish is complete
-                        System.out.println("MQTT Publish Complete");
-                    }
-                });
-                client.connect();
-                client.subscribe("heum/username", 0);
-                findUserName = userName;
-                userName = "";
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {//Called when a outgoing publish is complete
+                    System.out.println("MQTT Publish Complete");
+                }
+            });
+            client.connect();
+            client.subscribe(SUBSCRIBE_TOPIC, 0);
+            findUserName = userName;
+            userName = "";
 //                client.disconnect();
 //                System.out.println("Subscriber disconnect");
-            } catch (MqttException e) {
-                e.printStackTrace();
-            }
-            System.out.println("findUserName: " + findUserName);
-            return findUserName;
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
