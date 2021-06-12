d6b9508e8532455de16a0d005888d665b1324559
llshl <lsh970708@naver.com>
2021-06-13 05:45:57 +0900

    add IamportApiDTO

---
+++ src/main/java/com/jongsul/fabinetgradle/DTO/IamportApiDTO.java
@@ -0,0 +1,14 @@
+package com.jongsul.fabinetgradle.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class IamportApiDTO {
+    private String email;
+    private String name;
+    private String tel;
+    private long money;
+    private long num;
+}
