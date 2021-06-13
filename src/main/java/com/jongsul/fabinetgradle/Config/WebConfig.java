7e4d4ea45167e5bee28577f07ca738890cca6aa8
llshl <lsh970708@naver.com>
2021-06-14 03:32:54 +0900

    enable interceptor

--- src/main/java/com/jongsul/fabinetgradle/Config/WebConfig.java
+++ src/main/java/com/jongsul/fabinetgradle/Config/WebConfig.java
@@ -1,22 +1,24 @@
 package com.jongsul.fabinetgradle.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    //@Bean
+    @Bean
     public WebMvcConfigurer interceptorConfigure(){
         System.out.println("로그인 인터셉터 호출 - 로그인페이지로 이동");
         return new WebMvcConfigurer() {
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(new AuthInterceptor())
-                        .addPathPatterns("/boards")
-                        .addPathPatterns("/cabinets")
-                        .addPathPatterns("/adjustment")
+                        .addPathPatterns("/board")
+                        .addPathPatterns("/cabinet")
+                        .addPathPatterns("/bill")
                         .addPathPatterns("/upload")
+                        .addPathPatterns("/open")
                         .addPathPatterns("/mypage");
             }
         };
