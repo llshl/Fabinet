848ea88b81194e717a66fea641f68e2093ab720a
llshl <lsh970708@naver.com>
2021-06-16 04:31:15 +0900

    add member info

--- src/main/java/com/jongsul/fabinetgradle/Controller/MypageController.java
+++ src/main/java/com/jongsul/fabinetgradle/Controller/MypageController.java
@@ -1,11 +1,13 @@
 package com.jongsul.fabinetgradle.Controller;
 
 import com.jongsul.fabinetgradle.Config.MemberInformation;
+import com.jongsul.fabinetgradle.Domain.Board;
 import com.jongsul.fabinetgradle.Domain.Cabinet;
 import com.jongsul.fabinetgradle.Domain.CabinetHistory;
 import com.jongsul.fabinetgradle.Domain.Member;
 import com.jongsul.fabinetgradle.Service.CabinetHistoryService;
 import com.jongsul.fabinetgradle.Service.CabinetService;
+import com.jongsul.fabinetgradle.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpRequest;
@@ -26,11 +28,19 @@ public class MypageController {
 
     private final CabinetHistoryService cabinetHistoryService;
     private final MemberInformation memberInformation;
+    private final MemberService memberService;
 
     @GetMapping("/list")
     public ResponseEntity<List<CabinetHistory>> userCabinetList(HttpServletRequest request){
         String loginID = memberInformation.getUserName(request);
         List<CabinetHistory> allCabinet = cabinetHistoryService.getAllCabinet(loginID);
         return ResponseEntity.ok(allCabinet);
+    }
+
+    @GetMapping("/info")
+    public Member userInfo(HttpServletRequest request){
+        String loginID = memberInformation.getUserName(request);
+        Member findMember = memberService.findOne(loginID);
+        return findMember;
     }
 }
