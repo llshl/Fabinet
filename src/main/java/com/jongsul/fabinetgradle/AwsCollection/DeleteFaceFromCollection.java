936040f6f6143bdbd140f41ae0cb39cba1a54884
llshl <lsh970708@naver.com>
2021-07-15 01:07:25 +0900

    컬렉션 수정

--- src/main/java/com/jongsul/fabinetgradle/AwsCollection/DeleteFaceFromCollection.java
+++ src/main/java/com/jongsul/fabinetgradle/AwsCollection/DeleteFaceFromCollection.java
@@ -13,10 +13,7 @@ import java.util.List;
 public class DeleteFaceFromCollection {
     public static final String collectionId = "Collection";
     //컬랙션에서 지울 사진의 id
-    public static final String faces[] = {"06e1c661-eb36-46e6-bc16-ce8d50d095f6"
-    ,"8e019913-84a5-451a-96c7-0b0ca9d0be31"
-    ,"d5d31ab2-fe33-4295-af8f-943fe4619f92"
-    ,"dd73532f-ab09-4c57-b785-0372899f13c0"};
+    public static final String faces[] = {"e5319151-af08-44c2-9c49-67b068ae2d49"};
 
     public static void main(String[] args) throws Exception {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIA4EPX72XC3ACFOWVU", "BnW0X9nSPsqJN07KXgjKreizEa4Q7BI4I9Qdrytd");
